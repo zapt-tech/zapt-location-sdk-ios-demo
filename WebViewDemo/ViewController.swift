84c15b54b82b40754c974ce14fcbc02df21d4772
Bruno Carneiro <bruno@zapt.tech>
2025-08-06 16:23:23 -0300

    Viracopos - when clicking in a benner must open URL in a external instance of browser

--- WebViewDemo/ViewController.swift
+++ WebViewDemo/ViewController.swift
@@ -1,29 +1,61 @@
-//
-//  Copyright (c) 2024 Zapt Tech. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController, UIWebViewDelegate  {
-    
-    @IBOutlet var webView: UIWebView!
-    
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-                                
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let zaptLocation = appDelegate.zaptSDK
-        
-        let url = NSURL(string: zaptLocation?.getMapLink() ?? "")
-        let request = NSURLRequest(url: url! as URL)
-        
-        webView.delegate = self
-        webView.loadRequest(request as URLRequest)
-    }
+class ViewController: UIViewController, UIWebViewDelegate {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	@IBOutlet var webView: UIWebView!
+	let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+	var initialURL: URL?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		let zaptLocation = appDelegate.zaptSDK
+		let baseURL = URL(string: zaptLocation?.getMapLink() ?? "")
+		var finalURL: URL?
+
+		if let baseURL = baseURL {
+			if let sourceURL = initialURL {
+				finalURL = URL(string: appendQueryParameters(from: sourceURL.absoluteString, to: baseURL.absoluteString))
+			} else {
+				finalURL = baseURL
+			}
+
+			webView.delegate = self
+			let request = URLRequest(url: finalURL!)
+			webView.loadRequest(request)
+		}
+	}
+
+	func appendQueryParameters(from sourceURL: String, to destinationURL: String) -> String {
+		guard let sourceComponents = URL(string: sourceURL),
+			  let query = sourceComponents.query,
+			  !query.isEmpty else {
+			return destinationURL
+		}
+
+		let separator = destinationURL.contains("?") ? "&" : "?"
+		return destinationURL + separator + query
+	}
+
+	// MARK: - UIWebViewDelegate
+	func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
+		guard let url = request.url else { return false }
+
+		// Detecta se é uma tentativa de "window.open" de um link externo
+		if navigationType == .linkClicked || navigationType == .other {
+			if !isInternalURL(url) {
+				// Abre no navegador externo (Safari)
+				UIApplication.shared.open(url, options: [:], completionHandler: nil)
+				return false
+			}
+		}
+
+		return true
+	}
+
+	func isInternalURL(_ url: URL) -> Bool {
+		guard let host = url.host else { return false }
+		return host.contains("app.zapt.tech") || host.contains("maps.zapt.tech") || host.contains("firebase")
+	}
 }
-
--- WebViewDemoClip/ViewController.swift
+++ WebViewDemoClip/ViewController.swift
@@ -1,27 +1,19 @@
-//
-//  ViewController.swift
-//  WebViewDemoClip
-//
-//  Created by Bruno Carneiro on 05/05/25.
-//  Copyright © 2025 Ravi Shankar. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController, UIWebViewDelegate  {
-	
+class ViewController: UIViewController, UIWebViewDelegate {
+
 	@IBOutlet var webView: UIWebView!
 	let appDelegate = UIApplication.shared.delegate as! AppDelegate
-	
+
 	var initialURL: URL?
 
-								
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
 		let zaptLocation = appDelegate.zaptSDK
 		let baseURL = URL(string: zaptLocation?.getMapLink() ?? "")
 		var finalURL: URL?
-		
+
 		if let baseURL = baseURL {
 			if let sourceURL = initialURL {
 				finalURL = URL(string: appendQueryParameters(from: sourceURL.absoluteString, to: baseURL.absoluteString))
@@ -29,17 +21,12 @@ class ViewController: UIViewController, UIWebViewDelegate  {
 				finalURL = baseURL
 			}
 
-			let request = URLRequest(url: finalURL!)
 			webView.delegate = self
+			let request = URLRequest(url: finalURL!)
 			webView.loadRequest(request)
 		}
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-	
 	func appendQueryParameters(from sourceURL: String, to destinationURL: String) -> String {
 		guard let sourceComponents = URL(string: sourceURL),
 			  let query = sourceComponents.query,
@@ -47,10 +34,28 @@ class ViewController: UIViewController, UIWebViewDelegate  {
 			return destinationURL
 		}
 
-		// Verifica se a URL base já tem "?"
 		let separator = destinationURL.contains("?") ? "&" : "?"
 		return destinationURL + separator + query
 	}
 
-}
+	// MARK: - UIWebViewDelegate
+	func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
+		guard let url = request.url else { return false }
 
+		// Detecta se é uma tentativa de "window.open" de um link externo
+		if navigationType == .linkClicked || navigationType == .other {
+			if !isInternalURL(url) {
+				// Abre no navegador externo (Safari)
+				UIApplication.shared.open(url, options: [:], completionHandler: nil)
+				return false
+			}
+		}
+
+		return true
+	}
+
+	func isInternalURL(_ url: URL) -> Bool {
+		guard let host = url.host else { return false }
+		return host.contains("app.zapt.tech") || host.contains("maps.zapt.tech") || host.contains("firebase")
+	}
+}
