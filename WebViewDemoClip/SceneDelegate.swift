948a8da337a574f17d63bb3f889572c8eba5955b
Bruno Carneiro <bruno@zapt.tech>
2025-08-06 15:50:03 -0300

    ViracoposMaps - v1 working

--- WebViewDemoClip/SceneDelegate.swift
+++ WebViewDemoClip/SceneDelegate.swift
@@ -13,12 +13,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+
+		if let userActivity = connectionOptions.userActivities.first ?? session.stateRestorationActivity,
+		   let incomingURL = userActivity.webpageURL {
+			
+			// Instanciar o ViewController do storyboard
+			let storyboard = UIStoryboard(name: "Main", bundle: nil)
+			if let viewController = storyboard.instantiateInitialViewController() as? ViewController {
+				viewController.initialURL = incomingURL // envia a URL para o VC
+
+				let window = UIWindow(windowScene: windowScene)
+				window.rootViewController = viewController
+				self.window = window
+				window.makeKeyAndVisible()
+			}
+		}
+	}
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
--- WebViewDemoClip/ViewController.swift
+++ WebViewDemoClip/ViewController.swift
@@ -12,21 +12,44 @@ class ViewController: UIViewController, UIWebViewDelegate  {
 	
 	@IBOutlet var webView: UIWebView!
 	let appDelegate = UIApplication.shared.delegate as! AppDelegate
+	
+	var initialURL: URL?
+
 								
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		let zaptLocation = appDelegate.zaptSDK
+		let baseURL = URL(string: zaptLocation?.getMapLink() ?? "")
+		var finalURL: URL?
 		
-		let url = NSURL(string: zaptLocation?.getMapLink() ?? "")
-		let request = NSURLRequest(url: url! as URL)
-		
-		webView.delegate = self
-		webView.loadRequest(request as URLRequest)
+		if let baseURL = baseURL {
+			if let sourceURL = initialURL {
+				finalURL = URL(string: appendQueryParameters(from: sourceURL.absoluteString, to: baseURL.absoluteString))
+			} else {
+				finalURL = baseURL
+			}
+
+			let request = URLRequest(url: finalURL!)
+			webView.delegate = self
+			webView.loadRequest(request)
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+	func appendQueryParameters(from sourceURL: String, to destinationURL: String) -> String {
+		guard let sourceComponents = URL(string: sourceURL),
+			  let query = sourceComponents.query,
+			  !query.isEmpty else {
+			return destinationURL
+		}
+
+		// Verifica se a URL base já tem "?"
+		let separator = destinationURL.contains("?") ? "&" : "?"
+		return destinationURL + separator + query
 	}
 
 }
