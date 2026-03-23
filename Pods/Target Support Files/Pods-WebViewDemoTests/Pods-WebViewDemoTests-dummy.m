94c57ce8d73f3db1caffbc894272581fde67af49
Francisnei <francisnei@zapt.tech>
2026-03-23 17:30:13 -0300

    Track Podfile.lock; un-ignore for CocoaPods

--- Pods/Target Support Files/Pods-WebViewDemoTests/Pods-WebViewDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WebViewDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_WebViewDemoTests
-@end
--- Pods/ZaptLocation-iOS-SDK/README.md
+++
@@ -1,2 +0,0 @@
-# Zapt Location iOS SDK
-Indoor positioning for Navigation Systems, People Tracking & Asset Tracking
